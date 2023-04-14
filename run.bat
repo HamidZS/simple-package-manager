bd9ff9bcd569fe52cad7892e8bb73058c936d5c9
Hamid Zabihi <hamid.zbh@gmail.com>
2023-04-14 21:18:41 +0330

    initial

---
+++ run.bat
@@ -0,0 +1,7 @@
+echo "# simple-package-manager" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/HamidZS/simple-package-manager.git
+git push -u origin main
